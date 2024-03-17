eaf6f991b4579d057e69aac38ed9e5d827cce86e
Arthur <arthur20051208@yandex.ru>
2024-03-17 04:06:03 -0400

    complete go recangle module

---
+++ GO/module_rect.go
@@ -0,0 +1,15 @@
+package rect
+import (
+"math"
+)
+
+func square(x, y float64) float64{
+	return x * y}
+	
+func perimeter(x, y float64) float64{
+	return (x + y) * 2}
+	
+func diagon(x, y float64) float64{
+	var z float64
+	z = math.Sqrt(x * x + y * y)
+	return z}
