0c6d04c492026dd489a832eb7738925567da3ca4
Arthur <arthur20051208@yandex.ru>
2024-03-17 06:37:49 -0400

    added main for go program

--- GO/rect/module_rect.go
+++ GO/rect/module_rect.go
@@ -3,13 +3,13 @@ import (
 "math"
 )
 
-func square(x, y float64) float64{
+func Square(x, y float64) float64{
 	return x * y}
 	
-func perimeter(x, y float64) float64{
+func Perimeter(x, y float64) float64{
 	return (x + y) * 2}
 	
-func diagon(x, y float64) float64{
+func Diagon(x, y float64) float64{
 	var z float64
 	z = math.Sqrt(x * x + y * y)
 	return z}
--- GO/trian/module_trian.go
+++ GO/trian/module_trian.go
@@ -4,25 +4,25 @@ import (
 	"math"
 )
 
-func verification(a, b, c float64) bool {
+func Verification(a, b, c float64) bool {
 	if a <= 0 || b <= 0 || c <= 0 || a+b <= c || a+c <= b || b+c <= a {
 		return false
 	}
 	return true
 }
 
-func perimeter(a, b, c float64) float64 {
+func Perimeter(a, b, c float64) float64 {
 	p := a + b + c
 	return p
 }
 
-func area(a, b, c, p float64) float64 {
+func Area(a, b, c, p float64) float64 {
 	p = p / 2
 	s := math.Sqrt(p * (p - a) * (p - b) * (p - c))
 	return s
 }
 
-func equilateral(a, b, c float64) bool {
+func Equilateral(a, b, c float64) bool {
 	if a == b || b == c || c == a {
 		return true
 	}
